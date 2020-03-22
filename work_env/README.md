d6bd939abdfac3fab21be7d2c06c3871f8ab36c9
BaekkieSong <BaekkieSong@github.com>
2020-03-22 12:13:24 +0900

    [refactor] 스크립트 정리 및 가이드 작성

---
+++ work_env/README.md
@@ -0,0 +1,6 @@
+# Environment Settings For Develop Cpp
+## Install
+* cd ~/
+* git clone https://github.com/BaekkieSong/FP_TEST.git
+* cd FP_TEST/work_env
+* ./main.sh
--- work_env/main.sh
+++ work_env/main.sh
@@ -1,10 +1,10 @@
 git config --global user.email "BaekkieSong@github.com"
 git config --global user.name "BaekkieSong"
-cp .bashrc ~/
-cp .vimrc ~/
-./install_packages.sh
-./build_googletest.sh
-./build_fp_test_base.sh
-cd
+cp ./settings/bashrc ~/
+cp ./settings/vimrc ~/
+./settings/install_packages.sh
+./settings/build_googletest.sh
+./settings/build_fp_test_base.sh
+cd ~/
 ctags -R
 source ~/.bashrc
